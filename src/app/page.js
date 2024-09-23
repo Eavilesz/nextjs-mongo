14bedb98bbdb121a49d57a1a691f433c525a4704
Eavilesz <ernesto-av@hotmail.com>
2024-09-23 12:24:57 -0500

    using client

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,29 @@
-import { connectDB } from '@/libs/mongodb';
-import User from '@/models/user';
+// import { connectDB } from '@/libs/mongodb';
+// import User from '@/models/user';
 
-async function loadUsers() {
-  await connectDB();
-  const users = await User.find();
-  return users;
-}
+// async function loadUsers() {
+//   await connectDB();
+//   const users = await User.find();
+//   return users;
+// }
 
-async function HomePage() {
-  const users = await loadUsers();
+// async function HomePage() {
+//   const users = await loadUsers();
 
-  return <div>{users[3].name}</div>;
+//   return <div>{users[3].name}</div>;
+// }
+// export default HomePage;
+'use client';
+import { useEffect, useState } from 'react';
+
+function HomePage() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    fetch('/api/users')
+      .then((res) => res.json())
+      .then((data) => setUser(data[3]));
+  }, []);
+  return <div>{user.name}</div>;
 }
 export default HomePage;
